9ab661c91df8056690f5ad44bed935c96d0e9642
Abdul Jaleel Malik <abduljaleelmalik@Lions-Mac.local>
2012-04-11 13:46:32 +0400

    8

---
+++ SpringBall/AppSpecificValues.h
@@ -0,0 +1,9 @@
+
+//Leaderboard Category IDs
+#define kLeaderboardHighScores @"GHD645302"
+#define kLeaderboardCumulative @"02"
+
+//Achievement IDs
+//#define kAchievement1 @"ach_1"
+//#define kAchievement2 @"ach_2"
+
